21234bcc051d74b9b68771d02e366fd5f33d19c6
Christine Augustine <blacchoy@gmail.com>
2020-04-13 01:45:09 -0700

    add intial user and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 // business logic
 
+function Pizza (size, crust, sauce, cheeze, veg, meat) {
+  this.size = size;
+  this.crust = crust;
+  this.sauce = sauce;
+  this.cheeze = cheeze;
+  this.veg = veg;
+  this.meat = meat;
+  this.price = 0;
+}
+
+
 // user logic
+
+$(document).ready(function() {
+  $("#pizza").submit(function(event) {
+    event.preventDefault();
+    var size = $("input[name=size]:checked").val();
+    var crust = $("input[name=crust]:checked").val();
+    var sauce = $("input[name=sauce]:checked").val();
+    var cheeze = $("input[name =cheeze]:checked").val();
+    var veg = $("input[name=veg]:checked").val();
+    var meat = $("input[name=meat]:checked").val();
+    console.log(crust);
+  });
+});
